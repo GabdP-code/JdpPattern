a0d5e326594599ca444eaa9c2e0cc1ce84ff5afa
jamesdeperio <deperiojames20@gmail.com>
2017-08-02 11:12:39 +0800

    Update RetrofitInstance.kt

--- app/src/main/java/library/jdp/jdppattern/service/RetrofitInstance.kt
+++ app/src/main/java/library/jdp/jdppattern/service/RetrofitInstance.kt
@@ -1,21 +1,22 @@
-package library.jdp.jdplib.service
+package library.jdp.jdppattern.service
 
 import android.content.Context
-import library.jdp.jdplib.BuildConfig
+import library.jdp.jdppattern.BuildConfig
 import okhttp3.Cache
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import org.jetbrains.annotations.NotNull
+import retrofit2.Converter
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
-import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
 /**
  * Created by jamesdeperio on 6/20/2017.
  */
 
-abstract class RetrofitInstance : RetrofitContract {
-
+abstract class RetrofitService : RetrofitContract {
+@NotNull
     private lateinit var retrofit: Retrofit
     internal var header: RetrofitHeaderInterceptor = RetrofitHeaderInterceptor()
 
@@ -24,7 +25,7 @@ abstract class RetrofitInstance : RetrofitContract {
         val okHttpClient: OkHttpClient
         val cache = Cache(context.cacheDir, cacheSize.toLong())
         if (BuildConfig.DEBUG) {
-            val logging = HttpLoggingInterceptor()
+                val logging = HttpLoggingInterceptor()
             okHttpClient = OkHttpClient.Builder()
                     .cache(cache)
                     .writeTimeout(10, TimeUnit.SECONDS)
@@ -46,14 +47,16 @@ abstract class RetrofitInstance : RetrofitContract {
         retrofit = Retrofit.Builder()
                 .baseUrl(setBaseURL())
                 .client(okHttpClient)
-                .addConverterFactory(GsonConverterFactory.create())
+                .addConverterFactory(converterFactory())
                 .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
                 .build()
         return retrofit.create(service)
     }
-     fun addHeader(key: String, value: String) {
+
+    abstract fun converterFactory(): Converter.Factory?
+
+    fun addHeader(key: String, value: String) {
         header.key.add(key)
         header.value.add(value)
     }
 }
-
